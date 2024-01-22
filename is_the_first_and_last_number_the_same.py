48f192639928133853fbac4e80d4fdc8d305d2a7
unknown <emgcalangian@gmail.com>
2024-01-22 14:43:54 +0800

    added user input for elements and something to store the said list

--- is_the_first_and_last_number_the_same.py
+++ is_the_first_and_last_number_the_same.py
@@ -3,9 +3,13 @@
 # pseudo code
 
 # ask user for number of elements they want in the list
-desired_number_of_list = input("How many numbers of elements do you want in the list? ")
-print(desired_number_of_list)
+desired_number_of_list = int(input("How many numbers of elements do you want in the list? "))
+list = []
 
 # make user input said elements
+for i in range(desired_number_of_list):
+    elements = input("Input your element: ")
+    list.append(elements)
+print(list)
 # write the program to identify whether or not the first and last number of the list is the same
 
