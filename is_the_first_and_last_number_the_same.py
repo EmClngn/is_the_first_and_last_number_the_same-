b3577c54876276325d75b8ddc0236a599b402d2e
unknown <emgcalangian@gmail.com>
2024-01-22 14:32:48 +0800

    pseudo code

---
+++ is_the_first_and_last_number_the_same.py
@@ -0,0 +1,7 @@
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# pseudo code
+
+# ask user for number of elements they want in the list
+# make user input said elements
+# write the program to identify whether or not the first and last number of the list is the same
